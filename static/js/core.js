424ffbf895a2ae54529139d31300735dfa4d17b3
geolado <g3ol4d0@hotmail.com>
2015-08-05 01:49:07 -0300

    new animations

--- static/js/core.js
+++ static/js/core.js
@@ -4,15 +4,6 @@ function draw( d , o ) {
 	ctx.clearRect(0, 0, c.width, c.height);
 	ctx.save()
 	ctx.globalAlpha = parseInt(o)/100
-	
-	var ss = document.styleSheets;
-    for (var i = 0, max = ss.length; i < max; i++) {
-        if (ss[i].href == "static/styles/print_style.css") {
-        	console.log("wtf") ;
-            c.width = 2480;
-        	c.height = 3508;
-        }
-    }
 
 	for (var i = 0 ; i < parseInt(d) ; i++ ) {
 		ctx.beginPath();
@@ -52,10 +43,23 @@ function select_print(t) {
 	console.log(t) ;
 }
 
+
+function show_things() {
+	$('#wtf').css({"visibility": "visible"});
+	$('#wtf').addClass('animated bounceIn');
+	$('#reload').css({"visibility": "visible"});
+	$('#reload').addClass('animated bounceIn');
+	$('#download').css({"visibility": "visible"})
+	$('#download').addClass('animated bounceIn');
+	$('#print_it').css({"visibility": "visible"})
+	$('#print_it').addClass('animated bounceIn');
+	console.log("lol") ;
+}
+
 var canvas = document.getElementById("title_background");
 var c = canvas.getContext("2d");
 
-c.fillStyle = 'rgb(114,152,63)';
+c.fillStyle = '#C9915F';
 // c.roundRect(0, 0, 230, 80, {lowerLeft:10,lowerRight:10 , upperLeft:10 , upperRight:10}, true, false);
 c.beginPath() ;
 c.moveTo(10,0) ;
